2cca6d05e5bf92de9b6e160372d5652eb8d94236
ab-costa <costa.alessandroborges@gmail.com>
2022-03-21 16:38:50 -0300

    Script banco de dados

---
+++ dump.sql
@@ -0,0 +1,35 @@
+
+create table cliente(
+    id serial not null primary key,
+    nome varchar(120) not null,
+    endereco varchar(150) not null
+);
+
+create table entregador(
+    id serial not null primary key,
+    nome varchar(120) not null,
+    email varchar(100) not null unique,
+    telefone varchar(15),
+    senha text not null
+);
+
+create table pedido(
+    id serial not null primary key,
+    data date,
+    valor double precision not null,
+    status varchar(15) not null,
+    id_cliente integer not null,
+    id_entregador integer null,
+
+    constraint fk_cliente foreign key(id_cliente) references cliente(id),
+    constraint fk_entregador foreign key(id_entregador) references entregador(id)
+);
+
+create table geolocalizacao(
+    id serial not null primary key,
+    momento timestamp,
+    coordenadas text,
+    id_pedido integer not null,
+
+    constraint fk_pedido foreign key(id_pedido) references pedido(id)
+);
