7c0afff5ddc0a4aeff6897b3ae5bebe72e966c65
ab-costa <costa.alessandroborges@gmail.com>
2022-03-22 10:38:45 -0300

    feat: criacao do cabecalho das classes

---
+++ src/main/java/br/com/javai/projeto/model/Entregador.java
@@ -0,0 +1,10 @@
+package br.com.javai.projeto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "entregador")
+public class Entregador {
+
+}
---
+++ src/main/java/br/com/javai/projeto/model/Geolocalizacao.java
@@ -0,0 +1,10 @@
+package br.com.javai.projeto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "geolocalizacao")
+public class Geolocalizacao {
+
+}
