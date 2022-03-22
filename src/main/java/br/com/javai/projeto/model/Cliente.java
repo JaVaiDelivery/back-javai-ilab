e68209983f1def8e51076a830b66a1fa6ec5345c
ab-costa <costa.alessandroborges@gmail.com>
2022-03-22 11:37:01 -0300

    feat: implementacao da estrutura das classes com anoteções

--- src/main/java/br/com/javai/projeto/model/Cliente.java
+++ src/main/java/br/com/javai/projeto/model/Cliente.java
@@ -1,10 +1,49 @@
 package br.com.javai.projeto.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "cliente")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "nome", length = 120 ,nullable = false)
+	private String nome;
+	
+	@Column(name = "endereco", length = 150, nullable = false)
+	private String endereco;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
--- src/main/java/br/com/javai/projeto/model/Geolocalizacao.java
+++ src/main/java/br/com/javai/projeto/model/Geolocalizacao.java
@@ -1,10 +1,62 @@
 package br.com.javai.projeto.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "geolocalizacao")
 public class Geolocalizacao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "momento")
+	private Timestamp momento;
+	
+	@Column(name = "coordenadas", columnDefinition = "text")
+	private String coordenadas;
+	
+	@Column(name = "id_pedido", nullable = false)
+	private Integer idPedido;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Timestamp getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Timestamp momento) {
+		this.momento = momento;
+	}
+
+	public String getCoordenadas() {
+		return coordenadas;
+	}
+
+	public void setCoordenadas(String coordenadas) {
+		this.coordenadas = coordenadas;
+	}
+
+	public Integer getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Integer idPedido) {
+		this.idPedido = idPedido;
+	}
 }
