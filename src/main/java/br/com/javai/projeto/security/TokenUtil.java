e391c3debfeed79eeae4f88f84aa7cd7415c6dea
jfsax <jessicafsax@gmail.com>
2022-03-23 16:48:38 -0300

    Feature: criação dos métodos de controle do token

--- src/main/java/br/com/javai/projeto/security/TokenUtil.java
+++ src/main/java/br/com/javai/projeto/security/TokenUtil.java
@@ -1,5 +1,76 @@
 package br.com.javai.projeto.security;
 
-public class TokenUtil {
+import java.security.Key;
+import java.util.Collections;
+import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import br.com.javai.projeto.model.Entregador;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class TokenUtil {
+	private static final int SEGUNDOS = 1000;
+	private static final int MINUTOS = 60 * SEGUNDOS;
+	private static final int HORAS = 60 * MINUTOS;
+	private static final int DIAS = 24 * HORAS;
+	
+	private static final String HEADER = "Authorization";
+	private static final String PREFIX = "Bearer ";
+	private static final long EXPIRATION = 30 * MINUTOS;
+	private static final String SECRET_KEY = "123456789012345678901234567890 1";
+	private static final String EMISSOR = "JaVai";
+	
+	public static String criarToken(Entregador entregador) {
+		Key chaveSecreta = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+		
+		String token = Jwts.builder()
+				.setSubject(entregador.getEmail())
+				.setIssuer(EMISSOR)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+				.signWith(chaveSecreta, SignatureAlgorithm.HS256)
+				.compact();
+		
+		System.out.println("DEBUG = token gerado: " + token);
+		return PREFIX + token;
+	}
+	
+	private static boolean validarExpiracao(Date expiracao) {
+		return expiracao.after(new Date(System.currentTimeMillis()));
+	}
+	
+	private static boolean validarEmissor(String emissor) {
+		return emissor.equals(EMISSOR);
+	}
+	
+	private static boolean validarSubject(String email) {
+		return email != null && email.length() > 0;
+	}
+	
+	public static Authentication validar(HttpServletRequest req) {
+		String token = req.getHeader(HEADER);
+		token = token.replace(PREFIX, "");
+		
+		Jws<Claims> jwsClaims = Jwts.parserBuilder()
+				.setSigningKey(SECRET_KEY.getBytes())
+				.build()
+				.parseClaimsJws(token);
+		
+		String usuario = jwsClaims.getBody().getSubject();
+		String emissor = jwsClaims.getBody().getIssuer();
+		Date exp = jwsClaims.getBody().getExpiration();
+		
+		if (validarSubject(usuario) && validarEmissor(emissor) && validarExpiracao(exp)) {
+			return new UsernamePasswordAuthenticationToken(usuario, null, Collections.emptyList());
+		}
+		
+		return null;
+	}
 }
