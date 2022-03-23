cb05ccabde2a66123c558710dda68e68c71b843a
rvsfrebeca1 <rebecaferreiraprofissional@gmail.com>
2022-03-23 11:50:07 -0300

    Pacote DAO completo

---
+++ src/main/java/br/com/javai/projeto/dao/EntregadorDAO.java
@@ -0,0 +1,9 @@
+package br.com.javai.projeto.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.javai.projeto.model.Entregador;
+
+public interface EntregadorDAO extends CrudRepository<Entregador, Integer>{
+
+}
