5d43e09bfe170aa9ef7213f4346097197014a7a6
jfsax <jessicafsax@gmail.com>
2022-03-23 15:24:40 -0300

    Feature: criação do processo de criptografia da senha do login

--- src/main/java/br/com/javai/projeto/security/LoginCrypto.java
+++ src/main/java/br/com/javai/projeto/security/LoginCrypto.java
@@ -1,5 +1,25 @@
 package br.com.javai.projeto.security;
 
-public class LoginCrypto {
+import java.security.Key;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class LoginCrypto {
+	public static String encriptografar(String senha) throws Exception {
+		String strChave = "123456789012345678901234567890 1";
+		Key chave = new SecretKeySpec(strChave.getBytes(), "AES");
+		
+		Cipher cipher = Cipher.getInstance("AES");
+		
+		cipher.init(Cipher.ENCRYPT_MODE, chave);
+		
+		cipher.update(senha.getBytes());
+		
+		String senhaCripto = new String(cipher.doFinal(), "UTF-8");
+		
+		StringBuilder cryptoHex = new StringBuilder();
+		
+		return cryptoHex.toString();
+	}
 }
