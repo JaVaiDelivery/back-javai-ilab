f17d1cab0d2b62706998a8140780afbac8f15800
alluisi <lecaluisi@hotmail.com>
2022-03-24 14:32:06 -0300

    Feat: GET de pedidos e GET de pedidos/id

--- src/main/java/br/com/javai/projeto/controller/PedidoController.java
+++ src/main/java/br/com/javai/projeto/controller/PedidoController.java
@@ -1,8 +1,29 @@
 package br.com.javai.projeto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.javai.projeto.dao.PedidoDAO;
+import br.com.javai.projeto.model.Pedido;
 
 @RestController
 public class PedidoController {
 
+	@Autowired
+	private PedidoDAO dao;
+	
+	@GetMapping("/pedidos")
+	public List<Pedido> recuperarPedidosEmAberto() {
+		return dao.recuperarPedidosEmAberto();
+	}
+	
+	@GetMapping("/pedidos/{id}")
+	public List<Pedido> recuperarPedidoEspecifico(@PathVariable int id) {
+		return dao.recuperarPedidoEspecifico(id);
+	}
+	
 }
