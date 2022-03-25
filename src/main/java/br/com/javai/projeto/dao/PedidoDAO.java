003533c2bfb10047b6bcc5e7de1ea696d8d99be8
jfsax <jessicafsax@gmail.com>
2022-03-25 19:47:44 -0300

    Fix: correção dos annotations da query de update

--- src/main/java/br/com/javai/projeto/dao/PedidoDAO.java
+++ src/main/java/br/com/javai/projeto/dao/PedidoDAO.java
@@ -1,21 +1,26 @@
 package br.com.javai.projeto.dao;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.javai.projeto.model.Pedido;
+
 public interface PedidoDAO extends CrudRepository<Pedido, Integer>{
 	
 	@Query(value = "SELECT * FROM pedido WHERE pedido.status = 'Em aberto'", nativeQuery = true)
 	public List<Pedido> recuperarPedidosEmAberto();
 	
+	@Transactional
+	@Modifying
 	@Query("UPDATE Pedido as pedido"
-			+ " SET pedido.entregador = :idEntregador,"
+			+ " SET pedido.entregador.id = :idEntregador,"
 			+ " pedido.status = 'Em progresso'"
 			+ " WHERE pedido.id = :id")
-	public boolean atribuirEntregadorEMudarStatus(@Param("idEntregador") Integer idEntregador, @Param("id") Integer id);
+	public void atribuirEntregadorEMudarStatus(@Param("idEntregador") Integer idEntregador, @Param("id") Integer id);
 	
 	@Query("UPDATE Pedido as pedido"
 			+ " SET pedido.entregador = null,"
