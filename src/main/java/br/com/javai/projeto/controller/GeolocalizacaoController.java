fc45d61560c5abd4f4034e22336e9a14eba8ac22
jfsax <jessicafsax@gmail.com>
2022-03-25 18:42:25 -0300

    Feat: criação do POST do controlador Geolocalizacao

--- src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
+++ src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
@@ -1,26 +1,34 @@
 package br.com.javai.projeto.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.javai.projeto.dao.EntregadorDAO;
+import br.com.javai.projeto.dao.GeolocalizacaoDAO;
+import br.com.javai.projeto.dao.PedidoDAO;
 import br.com.javai.projeto.model.Geolocalizacao;
 
 @RestController
 public class GeolocalizacaoController {
 
 	@Autowired
-	private EntregadorDAO dao;
+	private GeolocalizacaoDAO dao;
 	
-	@GetMapping("/geolocalizacao/{idEntregador}")
-	public List<Geolocalizacao> receberGeolocalizacaoEntregador(@PathVariable Integer idEntregador){
-		List<Geolocalizacao> geos = dao.findAllById(idEntregador);
-		
-		return geos;
+	@Autowired
+	private PedidoDAO pedidoDao;
+	
+	@PostMapping("/geolocalizacao")
+	public Geolocalizacao receberGeolocalizacaoEntregador(@RequestBody Geolocalizacao geo){
+		System.out.println(geo.getEntregador().getId() + ", " + geo.getPedido().getId());
+		try {
+////			if (geo.getEntregador() != null && geo.getPedido() != null) {		
+//		pedidoDao.atribuirEntregadorEMudarStatus(geo.getEntregador(), geo.getPedido());
+//		
+			return dao.save(geo);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
-	
 }
