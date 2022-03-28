334c5b507286e755b6204d9f0755aff37192e1a5
jfsax <jessicafsax@gmail.com>
2022-03-28 14:01:51 -0300

    Feat: implementação do service de entregador

--- src/main/java/br/com/javai/projeto/services/IEntregadorService.java
+++ src/main/java/br/com/javai/projeto/services/IEntregadorService.java
@@ -3,6 +3,6 @@ package br.com.javai.projeto.services;
 import br.com.javai.projeto.dto.EntregadorLoginDTO;
 import br.com.javai.projeto.security.Token;
 
-public interface EntregadorService {
+public interface IEntregadorService {
 	public Token gerarTokenDeLogin(EntregadorLoginDTO dados);
 }
