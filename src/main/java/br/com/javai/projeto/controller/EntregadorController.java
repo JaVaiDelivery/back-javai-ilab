53b845b1a133950731e618ca662283858d0426e8
alluisi <lecaluisi@hotmail.com>
2022-03-28 16:21:43 -0300

    Fix: Refatoração dos controllers

--- src/main/java/br/com/javai/projeto/controller/EntregadorController.java
+++ src/main/java/br/com/javai/projeto/controller/EntregadorController.java
@@ -1,6 +1,7 @@
 package br.com.javai.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import br.com.javai.projeto.model.Entregador;
 import br.com.javai.projeto.security.LoginCrypto;
 
 @RestController
+@CrossOrigin("*")
 public class EntregadorController {
 
 	@Autowired
