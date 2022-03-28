ba2002e093427c432356dbd90ab2bdbcdc1a28b7
alluisi <lecaluisi@hotmail.com>
2022-03-28 11:28:40 -0300

    Fix: refatoração dos gets de pedido

--- src/main/java/br/com/javai/projeto/controller/PedidoController.java
+++ src/main/java/br/com/javai/projeto/controller/PedidoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +19,29 @@ public class PedidoController {
 	private PedidoDAO dao;
 	
 	@GetMapping("/pedidos")
-	public List<Pedido> recuperarPedidosEmAberto() {
-		return dao.recuperarPedidosEmAberto();
+	public ResponseEntity<List<Pedido>> recuperarPedidosEmAberto() {
+		try {
+			List<Pedido> pedidos = dao.recuperarPedidosEmAberto();
+		return ResponseEntity.ok(pedidos);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 	
 	@GetMapping("/pedidos/{id}")
-	public Optional<Pedido> recuperarPedidoEspecifico(@PathVariable int id) {
-		return dao.findById(id);
+	public ResponseEntity<?> recuperarPedidoEspecifico(@PathVariable int id) {
+		try {
+			Optional<Pedido> pedido = dao.findById(id);
+			if (pedido == null) {
+				return ResponseEntity.status(404).body("Pedido não encontrado!");
+				// TODO - Criar Util de respostas
+			}
+			return ResponseEntity.ok(pedido);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 	
 }
