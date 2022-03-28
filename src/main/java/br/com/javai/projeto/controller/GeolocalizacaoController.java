6a6b9f184bc9208bba0a034b168da66cd834b655
jfsax <jessicafsax@gmail.com>
2022-03-28 10:46:03 -0300

    Fix: refatoração do endpoint POST de geolocalizacao

--- src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
+++ src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
@@ -1,34 +1,34 @@
 package br.com.javai.projeto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.javai.projeto.dao.GeolocalizacaoDAO;
-import br.com.javai.projeto.dao.PedidoDAO;
 import br.com.javai.projeto.model.Geolocalizacao;
 
 @RestController
 public class GeolocalizacaoController {
-
+	
 	@Autowired
 	private GeolocalizacaoDAO dao;
 	
-	@Autowired
-	private PedidoDAO pedidoDao;
-	
 	@PostMapping("/geolocalizacao")
-	public Geolocalizacao receberGeolocalizacaoEntregador(@RequestBody Geolocalizacao geo){
-		try {
-			if (geo.getEntregador() != null && geo.getPedido() != null) {	
-				pedidoDao.atribuirEntregadorEMudarStatus(geo.getEntregador().getId(), geo.getPedido().getId());
-				
-				return dao.save(geo);
+	public ResponseEntity<Geolocalizacao> receberGeolocalizacaoEntregador(@RequestBody Geolocalizacao geo){
+		try {			
+			if (geo.getPedido() == null || geo.getPedido().getId() == null || geo.getEntregador() == null || geo.getEntregador().getId() == null) {
+				return ResponseEntity.status(400).build();
 			}
+			
+			Geolocalizacao nova = dao.save(geo);
+			
+			return ResponseEntity.ok(nova);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		
 		return null;
 	}
 }
