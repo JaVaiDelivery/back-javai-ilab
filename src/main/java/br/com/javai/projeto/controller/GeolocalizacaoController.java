82fddf8fba382c0dea711c006d67658ce58f01ae
rvsfrebeca1 <rebecaferreiraprofissional@gmail.com>
2022-03-30 17:42:07 -0300

    Fix: adicionando validacoes

--- src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
+++ src/main/java/br/com/javai/projeto/controller/GeolocalizacaoController.java
@@ -1,5 +1,7 @@
 package br.com.javai.projeto.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.javai.projeto.dao.EntregadorDAO;
 import br.com.javai.projeto.dao.GeolocalizacaoDAO;
+import br.com.javai.projeto.dao.PedidoDAO;
+import br.com.javai.projeto.model.Entregador;
 import br.com.javai.projeto.model.Geolocalizacao;
+import br.com.javai.projeto.model.Pedido;
 import br.com.javai.projeto.util.Message;
 
 @RestController
@@ -18,12 +24,32 @@ public class GeolocalizacaoController {
 	@Autowired
 	private GeolocalizacaoDAO dao;
 	
+	@Autowired
+	private PedidoDAO daoPedido;
+	
+	@Autowired
+	private EntregadorDAO daoEntregador;
+	
 	@PostMapping("/geolocalizacao")
 	public ResponseEntity<?> receberGeolocalizacaoEntregador(@RequestBody Geolocalizacao geo){
 		
-		try {			
+		
+		try {	
+			
 			if (geo.getPedido() == null || geo.getPedido().getId() == null || geo.getEntregador() == null || geo.getEntregador().getId() == null) {
-				return ResponseEntity.status(400).build();
+				return ResponseEntity.status(400).body(new Message("Todos os campos são obrigatórios"));
+			}
+			
+			Optional<Pedido> pedidoEncontrado = daoPedido.findById(geo.getPedido().getId());
+			Optional<Entregador> entregadorEncontrado = daoEntregador.findById(geo.getEntregador().getId());
+			
+			
+			if(pedidoEncontrado.isEmpty()) {
+				return (ResponseEntity<?>) ResponseEntity.badRequest().body(new Message("Pedido não encontrado"));
+			}
+			
+			if(entregadorEncontrado.isEmpty()) {
+				return (ResponseEntity<?>) ResponseEntity.badRequest().body(new Message("Entregador não encontrado"));
 			}
 			
 			Geolocalizacao nova = dao.save(geo);
