0c5e39833a6c1533922076abcd6f5ebbeeb53c3f
ab-costa <costa.alessandroborges@gmail.com>
2022-03-31 22:17:34 -0300

    fix: alteração das rotas públicas no API security

--- src/main/java/br/com/javai/projeto/security/APISecurityConfig.java
+++ src/main/java/br/com/javai/projeto/security/APISecurityConfig.java
@@ -20,12 +20,7 @@ public class APISecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/login").permitAll()
-		.antMatchers(HttpMethod.GET, "/pedidos").permitAll()
-		.antMatchers(HttpMethod.GET, "/pedidos/*").permitAll()
-		.antMatchers(HttpMethod.POST, "/geolocalizacao").permitAll()
-		.antMatchers(HttpMethod.GET, "/geolocalizacao/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/entregador").permitAll()
-		.antMatchers(HttpMethod.PATCH, "/pedidos/*").permitAll()
 		.antMatchers(HttpMethod.GET, "/v2/api-docs", "/configuration/ui", 
 										"/swagger-resources/**",  "/configuration/security",
 											"/swagger-ui.html", "/webjars/**").permitAll()
